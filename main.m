459597d3b3be6f3ebad99f70442a99f35c3dabad
jyyblue <jyyblue1987@outlook.com>
2021-05-08 01:23:45 +0800

    [add] draw plot.

--- main.m
+++ main.m
@@ -3,22 +3,23 @@ clc
 clear all
 
 % define paramters
-N = 4
+N = 60
 
-lambda = 1
+lambda = 0
 
 t = 0.5
 E0 = 2
 f = 1/3
 a = 10
-n = 1:N
+n = 1:N;
 
-step = int32(2*pi/0.01)
+x = [-pi:0.01:pi];
+step = length(x);
 res=zeros(N,step);
-k = 1;
+c = 1;
 
 for k = -pi:0.01:pi
-    r = E0 - 2*t*cos(2*pi*f*n + k*a);
+    r = E0 - 2*cos(2*pi*f*n + k*a);
     M = diag(r);
     for i = 2:N
         M(i, i - 1) = 1;
@@ -26,13 +27,21 @@ for k = -pi:0.01:pi
     end 
     M(N, 1) = lambda;
     M(1, N) = lambda;
-    if k > step
-        break
-    end
     
     [V, D] = eig(M);
     d = diag(D);
-    res(:, k) = d
+    res(:, c) = d;
     
-    k = k + 1;
+    c = c + 1;
 end
+
+disp('end of calculating eigenvalue data') 
+
+figure 
+for i =1:N
+    hold on;
+    plot(x, res(i,:))
+end
+
+
+
