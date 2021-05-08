f214f8575745f4e3ca3727f13390488ae726a876
jyyblue <jyyblue1987@outlook.com>
2021-05-08 17:23:05 +0800

    [add] show plot eigen vectors

---
+++ main1.m
@@ -0,0 +1,41 @@
+%% Generate Eignevector's graph
+clc 
+clear all
+
+% define paramters
+N = 60;
+
+lambda = 0;
+
+t = 0.5;
+E0 = 2;
+f = 1/3;
+a = 10;
+n = 1:N;
+
+c = 1;
+
+k = 0.5;
+r = E0 - 2*t*cos(2*pi*f*n + k*a);
+M = diag(r);
+for i = 2:N
+    M(i, i - 1) = 1;
+    M(i - 1, i) = 1;
+end 
+M(N, 1) = lambda;
+M(1, N) = lambda;
+
+[V, D] = eig(M);
+
+d = [19 20 21 22]
+
+figure 
+
+for i =1:length(d)
+    hold on;
+    plot(V(:,d(i)));
+end
+
+
+
+
